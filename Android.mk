d2774a475caad77d04386f6d3e90292e70eac7c6
Sultanxda <sultanxda@gmail.com>
2017-07-09 13:57:42 -0700

    Use official LineageOS apks
    Pulled from https://download.lineageos.org/extras

--- Android.mk
+++ Android.mk
@@ -15,8 +15,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := OpenWeatherMapProvider
-LOCAL_SRC_FILES := OpenWeatherMapProvider.apk
+LOCAL_MODULE := OpenWeatherMapWeatherProvider
+LOCAL_SRC_FILES := OpenWeatherMapWeatherProvider.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
